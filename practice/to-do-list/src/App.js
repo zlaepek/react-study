e472384835f3733ccd42d9b5169e6f3316261886
zlaepek <69634389+zlaepek@users.noreply.github.com>
2022-02-07 10:45:10 +0900

    Add files via upload

---
+++ practice/to-do-list/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    console.log(toDos);
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo("");
+
+  }
+  return (
+    <div className="App">
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo}
+          type="text" 
+          placeholder='Write your to do...' 
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item) => 
+        <li key={index}>{item}</li>
+      )}
+    </div>
+  );
+}
+
+export default App;
